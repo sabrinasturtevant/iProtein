c167d8303cad137b05fbf8a11b0cf2f78d8523e6
Sabrina Sturtevant <sabrinasturtevant@Sabrinas-MacBook-Pro.local>
2019-06-27 11:54:07 -0500

    readjusted scroll view. attempting to add background colors on selected symbols for ligand screen. lunch break

--- iProtein/ViewController.swift
+++ iProtein/ViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
+var singleBond = 0
+var doubleBond = 0
+var tripleBond = 0
+var towardBond = 0
+var awayBond = 0
+var fiveSided = 0
+var sixSided = 0
+var sevenSided = 0
+var eightSided = 0
+
 
 class ViewController: UIViewController {
     
-    //attempting scroll view plus stack view
+    //-----outlets for scroll view and stack view
     
     @IBOutlet weak var stackViewLigandSymbols: UIStackView!
     
     @IBOutlet weak var scrollViewLigandSymbols: UIScrollView!
     
-
     
-    ///////initial segues from home screen
+    //-----initial segues from home screen
     @IBAction func makeLigandTapped(_ sender: Any) {
         performSegue(withIdentifier: "makeLigandSegue", sender: nil)
     }
@@ -37,7 +46,7 @@ class ViewController: UIViewController {
     }
     
     
-    ////////segues to go back to home screen
+    //------segues to go back to home screen
     @IBAction func homeTapped(_ sender: Any) {
         performSegue(withIdentifier: "homeSegue", sender: nil)
     }
@@ -48,9 +57,71 @@ class ViewController: UIViewController {
     
     
     
+    //-----outlets for ligand symbols
+       @IBOutlet weak var singleBondOutlet: UIImageView!
+    
+      @IBOutlet weak var doubleBondOutlet: UIImageView!
+    
+    //------actions for single bond
+    @IBAction func singleBondTapped(_ sender: Any) {
+        singleBond = 1
+        doubleBond = 0
+        tripleBond = 0
+        towardBond = 0
+        awayBond = 0
+        fiveSided = 0
+        sixSided = 0
+        sevenSided = 0
+        eightSided = 0
+        
+        if singleBond == 1 {
+            singleBondOutlet.backgroundColor = UIColor.gray.withAlphaComponent(0.5)
+        }
+        
+        if singleBond == 2 {
+            singleBond = 1
+        }
+    }
+
+    
+//-----actions and outlets for double bonds
+    
+    @IBAction func doubleBondTapped(_ sender: Any) {
+        doubleBondOutlet.backgroundColor = UIColor.gray.withAlphaComponent(0.5)
+        singleBond = 0
+        doubleBond = 1
+        tripleBond = 0
+        towardBond = 0
+        awayBond = 0
+        fiveSided = 0
+        sixSided = 0
+        sevenSided = 0
+        eightSided = 0
+        
+        if doubleBond == 1 {
+            doubleBondOutlet.backgroundColor = UIColor.gray.withAlphaComponent(0.5)
+        }
+        
+        if doubleBond == 2 {
+            doubleBond = 1
+    }
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         self.scrollViewLigandSymbols?.contentSize = CGSize(width: 320, height: 1000)
+        
+    self.scrollViewLigandSymbols?.isDirectionalLockEnabled == true
+
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
